d5ab299a91b59522d5416e9fa34e4ce925700ecc
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2023-09-29 22:12:02 +0000

    Set up done

---
+++ README.md
@@ -0,0 +1,3 @@
+# Acme Rockets Clone
+
+## I'm clonning this Acme Rocket website as a means to learn tailwind
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./buildl*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
